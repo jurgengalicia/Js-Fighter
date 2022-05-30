101202b6c0bd0fe4e4c46a1b1db1cb29904dc71d
jurgengalicia <jurgen.galicia@gmail.com>
2022-05-30 14:30:43 -0700

    movement adjustments for P1

--- script.js
+++ script.js
@@ -43,19 +43,58 @@ const player2 = new Sprite({
     velocity:{x:0,y:0}
 });
 
+const keys = {
+    a:{pressed: false},
+    d:{pressed: false},
+    w:{pressed: false},
+}
+let lastKey;
+
 function animate(){
     window.requestAnimationFrame(animate);
     c.fillStyle = 'black';
     c.fillRect(0,0,canvas.width, canvas.height);
     player1.update();
     player2.update();
+
+    player1.velocity.x = 0
+    if(keys.a.pressed && lastKey === 'a'){
+        player1.velocity.x = -1;
+    }else if(keys.d.pressed && lastKey === 'd'){
+        player1.velocity.x = 1
+    }
 }
 
 animate();
 window.addEventListener('keydown', (event) =>{
     switch(event.key){
         case 'd':
-        player1.velocity.x = 1;
+            keys.d.pressed = true;
+            lastKey ='d'
+        break;
+        case 'a':
+            keys.a.pressed = true;
+            lastKey ='a'
+        break;
+        case 'w':
+            player1.velocity.y = -10;
+        break;
+    }
+    console.log(event);
+
+});
+
+window.addEventListener('keyup', (event) =>{
+    switch(event.key){
+        case 'd':
+            keys.d.pressed = false;
+        break;
+        case 'a':
+            keys.a.pressed = false;
+        break;
+        case 'w':
+            keys.w.pressed = false;
+            lastKey ='w'
         break;
     }
     console.log(event);
